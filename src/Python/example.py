6fe38390b1c2c407c8b6b7a07322d1c05ddfe9f2
grlaura <lauraribeiro.g@gmail.com>
2024-07-08 14:54:22 +0300

    Add: LoadAndShowImg.py and sample data

---
+++ src/Python/example.py
@@ -0,0 +1,8 @@
+import cv2
+import os
+from loadAndShowImg import loadAndShowImg
+# Call the function with the path to your image file
+# Define a function to load and show an image
+image_path = '../../data/sample/image_003.png';
+loadAndShowImg(image_path);
+
---
+++ src/Python/loadAndShowImg.py
@@ -0,0 +1,21 @@
+def loadAndShowImg(image_path):
+    import os
+    import cv2
+
+    # Load the image
+    image = cv2.imread(image_path)
+
+    # Check if the file exists
+    if os.path.isfile(image_path):
+        # Try to open the image
+        image = cv2.imread(image_path)
+        if image is not None:
+            print('Image loaded successfully')
+            cv2.imshow('Image', image)
+            # Wait for a key press to close the window
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+        else:
+            print('Image is not loaded, the file may be corrupted or in an unsupported format')
+    else:
+        print('File does not exist at the specified path')
